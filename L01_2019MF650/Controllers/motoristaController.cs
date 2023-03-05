ff9b601fee2453dec7f707ceea5f70d89b21aa12
Leoni <Leoni@WARMACHINE>
2023-03-04 20:02:14 -0600

    ENTREGA FINAL

--- L01_2019MF650/Controllers/motoristaController.cs
+++ L01_2019MF650/Controllers/motoristaController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using L01_2019MF650.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace L01_2019MF650.Controllers
 {
@@ -7,5 +9,104 @@ namespace L01_2019MF650.Controllers
     [ApiController]
     public class motoristaController : ControllerBase
     {
+        private readonly restauranteContext _restauranteContexto;
+
+        public motoristaController(restauranteContext restauranteContexto)
+        {
+            _restauranteContexto = restauranteContexto; ;
+        }
+
+        //Metodos Motorista
+        [HttpGet]
+        [Route("GetAll")]
+
+        public IActionResult Get()
+        {
+            List<motoristas> listadoMotor = (from e in _restauranteContexto.motoristas
+                                           select e).ToList();
+
+            if (listadoMotor.Count == 0)
+            {
+                return NotFound();
+            }
+            return Ok(listadoMotor);
+
+        }
+
+        [HttpGet]
+        [Route("GetByNombre/{name}")]
+
+        public IActionResult GetNombre(string name)
+        {
+            List<motoristas> listadoMotor = (from e in _restauranteContexto.motoristas
+                                           where e.nombreMotorista == name
+                                           select e).ToList();
+
+            if (listadoMotor.Count == 0)
+            {
+                return NotFound();
+            }
+            return Ok(listadoMotor);
+
+        }
+
+        [HttpPost]
+        [Route("Add")]
+
+        public IActionResult GuardarPedido([FromBody] motoristas motoristas)
+        {
+
+            try
+            {
+                _restauranteContexto.motoristas.Add(motoristas);
+                _restauranteContexto.SaveChanges();
+                return Ok(motoristas);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+
+            }
+
+        }
+
+        [HttpPut]
+        [Route("actualizar/{id}")]
+
+        public IActionResult ActualizarMotorista(int id, [FromBody] motoristas motorModificar)
+        {
+            motoristas? motoristaActual = (from e in _restauranteContexto.motoristas
+                                     where e.motoristaId == id
+                                     select e).FirstOrDefault();
+            if (motoristaActual == null)
+            {
+                return NotFound(id);
+            }
+
+            motoristaActual.nombreMotorista = motorModificar.nombreMotorista;
+
+            _restauranteContexto.Entry(motoristaActual).State = EntityState.Modified;
+            _restauranteContexto.SaveChanges();
+            return Ok(motorModificar);
+        }
+
+        [HttpDelete]
+        [Route("eliminar/{id}")]
+        public IActionResult EliminarMotorista(int id)
+        {
+
+            motoristas? motorista = (from e in _restauranteContexto.motoristas
+                               where e.motoristaId == id
+                               select e).FirstOrDefault();
+
+            if (motorista == null)
+                return NotFound();
+
+            _restauranteContexto.motoristas.Attach(motorista);
+            _restauranteContexto.motoristas.Remove(motorista);
+            _restauranteContexto.SaveChanges();
+
+            return Ok(motorista);
+        }
     }
 }
---
+++ L01_2019MF650/Controllers/platosController.cs
@@ -0,0 +1,114 @@
+﻿using L01_2019MF650.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace L01_2019MF650.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class platosController : ControllerBase
+    {
+        private readonly restauranteContext _restauranteContexto;
+
+        public platosController(restauranteContext restauranteContexto)
+        {
+            _restauranteContexto = restauranteContexto; ;
+        }
+        //Metodos Platos
+        [HttpGet]
+        [Route("GetAll")]
+
+        public IActionResult Get()
+        {
+            List<platos> listadoPlatos = (from e in _restauranteContexto.platos
+                                           select e).ToList();
+
+            if (listadoPlatos.Count == 0)
+            {
+                return NotFound();
+            }
+            return Ok(listadoPlatos);
+
+        }
+
+        [HttpGet]
+        [Route("GetByPrecio/{precio}")]
+
+        public IActionResult GetPrecios(decimal precio)
+        {
+            List<platos> listadoPlatos = (from e in _restauranteContexto.platos
+                                           where e.precio < precio
+                                           select e).ToList();
+
+            if (listadoPlatos.Count == 0)
+            {
+                return NotFound();
+            }
+            return Ok(listadoPlatos);
+
+        }
+
+
+        [HttpPost]
+        [Route("Add")]
+
+        public IActionResult GuardarPedido([FromBody] platos platos)
+        {
+
+            try
+            {
+                _restauranteContexto.platos.Add(platos);
+                _restauranteContexto.SaveChanges();
+                return Ok(platos);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+
+            }
+
+        }
+
+        [HttpPut]
+        [Route("actualizar/{id}")]
+
+        public IActionResult ActualizarPlato(int id, [FromBody] platos platoModificar)
+        {
+            platos? platoActual = (from e in _restauranteContexto.platos
+                                     where e.platoId == id
+                                     select e).FirstOrDefault();
+            if (platoActual == null)
+            {
+                return NotFound(id);
+            }
+
+            platoActual.nombrePlato = platoModificar.nombrePlato;
+            platoActual.precio = platoModificar.precio;
+
+            _restauranteContexto.Entry(platoActual).State = EntityState.Modified;
+            _restauranteContexto.SaveChanges();
+            return Ok(platoModificar);
+        }
+
+        [HttpDelete]
+        [Route("eliminar/{id}")]
+
+        public IActionResult EliminarPlato(int id)
+        {
+
+            platos? plato = (from e in _restauranteContexto.platos
+                               where e.platoId == id
+                               select e).FirstOrDefault();
+
+            if (plato == null)
+                return NotFound();
+
+            _restauranteContexto.platos.Attach(plato);
+            _restauranteContexto.platos.Remove(plato);
+            _restauranteContexto.SaveChanges();
+
+            return Ok(plato);
+        }
+    }
+}
